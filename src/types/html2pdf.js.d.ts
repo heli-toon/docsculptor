b10abf3ee75c8dcdd9a089d8bf7617d8feac2652
Salay Abdul Muhaimin Kanton <abdulkanton2005@gmail.com>
2026-04-24 17:48:39 +0000

    fix: build & type errors fixed

--- src/types/html2pdf.js.d.ts
+++ src/types/html2pdf.js.d.ts
@@ -1,4 +1,19 @@
 declare module 'html2pdf.js' {
-  const html2pdf: (element: HTMLElement | string, options?: object) => Promise<void>;
+  interface Html2PdfWorker {
+    from(element: HTMLElement | string): Html2PdfWorker;
+    set(options: any): Html2PdfWorker;
+    toPdf(): Html2PdfWorker;
+    toImg(): Html2PdfWorker;
+    toCanvas(): Html2PdfWorker;
+    toContainer(): Html2PdfWorker;
+    get(name: string): any;
+    then(callback: (pdf: any) => void): Html2PdfWorker;
+    save(filename?: string): Promise<void>;
+    output(type: string, options?: any): any;
+  }
+
+  function html2pdf(): Html2PdfWorker;
+  function html2pdf(element: HTMLElement | string, options?: any): Html2PdfWorker;
+
   export default html2pdf;
 }
